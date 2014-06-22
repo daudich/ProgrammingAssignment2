ee60c8c8a015323bc481be63e45accc6afd4e2bc
Dhiren Audich <audich_dhiren@hotmail.com>
2014-06-22 17:16:33 -0400

    saving changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions 
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+
+        inverse <- NULL ##initialising the default value for the inverted matrix
+        
+        ##initialising the value of the matrix that has to be inverted
+        set <- function(newMatrix) {
+                x <<- newMatrix
+                inverse <<- NULL
+        }
+        
+        ##this method returns the original matix
+        get <- function() x
+        
+        ##setting the value for the inverted matrix
+        setInvMatrix <- function(invMatrx) {
+		inverse <<- invMatrix
+	}
+        
+        ##this method returns the inverted matrix
+        getInvMatrix <- function() inverse
+        
+        ##listing all the methods available to this function object
+        list(set = set, get = get,
+             setInvMatrix = setInvMatrix,
+             getInvMatrix = getInvMatrix)
 
 }
 
@@ -12,4 +35,20 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        m <- x$getmean()
+        
+        if(!is.null(m)) {
+                message("Getting cached matrix.")
+                return(m)
+        }
+        
+        data <- x$get()
+        
+        m <- mean(data, ...)
+        
+        x$setmean(m)
+        
+        m
+        
 }
